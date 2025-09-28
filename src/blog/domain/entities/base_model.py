25447878c15bb114e826bf0105632f44a3fad515
Ksenia <kseniatravnikova@duck.com>
2025-09-28 19:03:14 +0300

    base model for entities

---
+++ src/blog/domain/entities/base_model.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass, field
+
+from bson import ObjectId
+
+
+@dataclass
+class BaseModel:
+    """Базовая модель для работы."""
+
+    _id: ObjectId | str = field(default_factory=ObjectId, kw_only=True)
+
+    @property
+    def id(self) -> str:
+        """Возвращает ID в человеко читаемом виде."""
+        return str(self._id)
