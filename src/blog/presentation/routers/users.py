05f6340d10dd2ddf61aaa1d7cf27160960f654ff
Ksenia <kseniatravnikova@duck.com>
2025-10-02 11:52:35 +0300

    route for deleting user

--- src/blog/presentation/routers/users.py
+++ src/blog/presentation/routers/users.py
@@ -3,9 +3,11 @@
 from litestar import Controller, Request, get, post
 from litestar.datastructures import State
 from litestar.exceptions import NotAuthorizedException
+from punq import Container
 
 from blog.application.dto.users import UserDTO
-from blog.domain.entities import User
+from blog.application.services.user_service import UserService
+from blog.domain.entities.user import User
 
 
 class UserController(Controller):
@@ -34,14 +36,29 @@ class UserController(Controller):
     )
     async def update_user(
         self,
-        data: UserDTO,
+        data: User,
         request: Request[User, str, State],
+        container: Container,
     ) -> User:
         """Обновление информации о пользователе."""
         user = request.user
         if not user:
             raise NotAuthorizedException
 
-        user_service = request.app.state.user_service
+        user_service = container.resolve(UserService)
         user = await user_service.update_user(user, data.__dict__)
         return user
+
+    @post(
+        path="/delete",
+        security=[{"BearerAuth": []}],
+    )
+    async def delete_user(
+        self, request: Request[User, str, State], container: Container
+    ) -> bool:
+        """Удаление информации о пользователе"""
+        user = request.user
+        if not user:
+            raise NotAuthorizedException
+        user_service = container.resolve(UserService)
+        return await user_service.delete_user(user)
