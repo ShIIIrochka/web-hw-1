fc7648a128991af52a90bac80c7abcbb315dfbb0
Ksenia <kseniatravnikova@duck.com>
2025-10-02 21:33:01 +0300

    posts controller

---
+++ src/blog/presentation/routers/posts.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from litestar import Controller, Request, delete, get, post, put
+from litestar.datastructures import State
+from litestar.dto import DataclassDTO
+from litestar.exceptions import (
+    NotAuthorizedException,
+    PermissionDeniedException,
+)
+from litestar.status_codes import HTTP_204_NO_CONTENT
+from punq import Container
+
+from blog.application.services.post_service import PostService
+from blog.domain.entities.post import Post
+from blog.domain.entities.user import User
+from blog.presentation.dto.posts import CreatePostDTO, PostDTO, UpdatePostDTO
+
+
+class PostController(Controller):
+    path = "/posts"
+    tags = ["Posts"]
+
+    @get("/{post_id:str}", response_dto=PostDTO)
+    async def get_post(
+        self,
+        post_id: str,
+        container: Container,
+    ) -> Post:
+        """Получение поста по ID."""
+        post_service: PostService = container.resolve(PostService)
+        return await post_service.get_post_by_id(post_id)
+
+    @post(
+        "/create",
+        dto=DataclassDTO[CreatePostDTO],
+        response_dto=PostDTO,
+        security=[{"BearerAuth": []}],
+    )
+    async def create_post(
+        self,
+        data: CreatePostDTO,
+        container: Container,
+        request: Request[User, str, State],
+    ) -> Post:
+        """Создание нового поста."""
+        post_service: PostService = container.resolve(PostService)
+        user = request.user
+        if not user:
+            raise NotAuthorizedException
+        post = await post_service.create_post(user, data.__dict__)
+        return post
+
+    @put(
+        "/{post_id:str}/update",
+        dto=DataclassDTO[UpdatePostDTO],
+        response_dto=PostDTO,
+        security=[{"BearerAuth": []}],
+    )
+    async def update_post(
+        self,
+        post_id: str,
+        data: UpdatePostDTO,
+        container: Container,
+        request: Request[User, str, State],
+    ) -> Post:
+        """Обновление поста."""
+        post_service: PostService = container.resolve(PostService)
+        post = await post_service.get_post_by_id(post_id)
+        try:
+            return await post_service.update_post(
+                request.user, post, data.__dict__
+            )
+        except PermissionError:
+            raise PermissionDeniedException
+
+    @delete(
+        "/{post_id:str}/delete",
+        status_code=HTTP_204_NO_CONTENT,
+        security=[{"BearerAuth": []}],
+    )
+    async def delete_post(
+        self,
+        post_id: str,
+        container: Container,
+        request: Request[User, str, State],
+    ) -> None:
+        """Удаление поста."""
+        post_service: PostService = container.resolve(PostService)
+        post = await post_service.get_post_by_id(post_id)
+        try:
+            await post_service.delete_post(request.user, post)
+        except PermissionError:
+            raise PermissionDeniedException
