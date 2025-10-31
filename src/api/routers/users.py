49ee3ab7a6527936e933ec3ea6546c1993389840
Ksenia <kseniatravnikova@duck.com>
2025-10-31 19:41:00 +0300

    refactor user controller

--- src/api/routers/users.py
+++ src/api/routers/users.py
@@ -3,11 +3,12 @@
 from litestar import Controller, Request, get, post, put
 from litestar.datastructures import State
 from litestar.dto import DTOData
-from litestar.exceptions import NotAuthorizedException, ValidationException
+from litestar.exceptions import ValidationException
 from litestar.status_codes import HTTP_200_OK, HTTP_204_NO_CONTENT
 from punq import Container
 
 from src.api.dto.users import UpdateUserDTO, UserDTO
+from src.api.guards.auth import auth_guard
 from src.application.services.user_service import UserService
 from src.domain.entities.user import User
 from src.domain.exceptions.user import EmailSyntaxError
@@ -18,6 +19,7 @@ class UserController(Controller):
 
     path = "/users"
     tags = ["Users"]
+    guards = [auth_guard]
 
     @get(
         path="/me",
@@ -25,12 +27,9 @@ class UserController(Controller):
         status_code=HTTP_200_OK,
         security=[{"BearerAuth": []}],
     )
-    async def get_user(self, request: Request[User, str, State]) -> User:
-        """Получение информации о пользователе."""
-        user = request.user
-        if not user:
-            raise NotAuthorizedException
-        return user
+    async def get_me(self, request: Request[User, str, State]) -> User:
+        """Получение информации о текущем пользователе."""
+        return request.user
 
     @put(
         path="/update",
@@ -46,13 +45,9 @@ class UserController(Controller):
         container: Container,
     ) -> User:
         """Обновление информации о пользователе."""
-        user = request.user
-        if not user:
-            raise NotAuthorizedException
-
         user_service = container.resolve(UserService)
         try:
-            updated_user = await user_service.update_user(user, data)
+            updated_user = await user_service.update_user(request.user, data)
         except EmailSyntaxError:
             raise ValidationException(detail="Invalid email format.")
         return updated_user
@@ -61,13 +56,12 @@ class UserController(Controller):
         path="/delete",
         status_code=HTTP_204_NO_CONTENT,
         security=[{"BearerAuth": []}],
+        response_cookies={"token": ""},
+        response_headers={},
     )
     async def delete_user(
         self, request: Request[User, str, State], container: Container
     ) -> None:
         """Удаление информации о пользователе"""
-        user = request.user
-        if not user:
-            raise NotAuthorizedException
         user_service = container.resolve(UserService)
-        await user_service.delete_user(user)
+        await user_service.delete_user(request.user.id)
