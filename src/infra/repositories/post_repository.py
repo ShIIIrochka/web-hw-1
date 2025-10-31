4bc9e43f071a2994b8e1b5f9263276cba829616d
Ksenia <kseniatravnikova@duck.com>
2025-10-31 19:45:11 +0300

    fix post repository

--- src/infra/repositories/post_repository.py
+++ src/infra/repositories/post_repository.py
@@ -5,6 +5,7 @@ from uuid import UUID
 
 from src.domain.entities.post import Post
 from src.domain.repositories.post_repository import BasePostRepository
+from src.infra.models import Category as CategoryModel
 from src.infra.models import Post as PostModel
 
 
@@ -14,50 +15,55 @@ class PostRepository(BasePostRepository):
     def __init__(
         self,
         model: type[PostModel] = PostModel,
+        category_model: type[CategoryModel] = CategoryModel,
     ) -> None:
         """Конструктор.
 
         Args:
             model (type[PostModel]): Модель поста
+            category_model (type[CategoryModel]): Модель категории
         """
         self._model = model
+        self._category_model = category_model
 
     async def add(self, data: Post) -> str:
         """Добавление поста в БД."""
         post_data = asdict(data)
-        post_data.pop("id", None)
         categories = post_data.pop("categories")
 
         new_post = await self._model.create(**post_data)
 
         if categories:
-            category_ids = [cat.id for cat in categories]
-            if category_ids:
-                await new_post.categories.add(*category_ids)
+            for cat in categories:
+                category = await self._category_model.get_or_none(id=cat)
+                if category:
+                    await new_post.categories.add(category)
+                else:
+                    raise ValueError
 
         return str(new_post.id)
 
-    async def get_by_id(self, id: str) -> Post | None:
+    async def get_by_id(self, id: UUID) -> Post | None:
         """Получение конкретного поста из БД."""
-        post = await self._model.get_or_none(id=UUID(id))
+        post = await self._model.get_or_none(id=id)
         if not post:
             return None
-        return await post.to_entity(include_categories=True)
+        return await post.to_entity()
 
     async def get_many(
-        self, cursor: str | None = None, limit: int = 10
+        self, cursor: UUID | None = None, limit: int = 10
     ) -> list[Post]:
         """Получение N объектов из БД."""
         query = self._model.all().order_by("created_at")
         if cursor:
-            query = query.filter(self._model.id > UUID(cursor))
+            query = query.filter(id__gt=cursor)
         posts = await query.limit(limit)
         result = []
         for post in posts:
-            result.append(await post.to_entity(include_categories=True))
+            result.append(await post.to_entity())
         return result
 
-    async def update(self, id: str, update_data: Post) -> Post:
+    async def update(self, id: UUID, update_data: Post) -> Post:
         """Обновление поста."""
         update_dict = asdict(update_data)
         update_dict.pop("id", None)
@@ -65,18 +71,23 @@ class PostRepository(BasePostRepository):
 
         await self._model.filter(id=id).update(**update_dict)
 
-        post = await self._model.get_or_none(id=UUID(id))
+        post = await self._model.get_or_none(id=id)
 
+        if not post:
+            raise ValueError
         if categories:
             await post.fetch_related("categories")
             await post.categories.clear()
-            category_ids = [cat.id for cat in categories]
-            if category_ids:
-                await post.categories.add(*category_ids)
+            for cat in categories:
+                category = await self._category_model.get_or_none(id=cat)
+                if category:
+                    await post.categories.add(category)
+                else:
+                    raise ValueError
 
-        return await post.to_entity(include_categories=True)
+        return await post.to_entity()
 
-    async def delete(self, id: str) -> bool:
+    async def delete(self, id: UUID) -> bool:
         """Удаление объекта."""
-        deleted_count = await self._model.filter(id=UUID(id)).delete()
+        deleted_count = await self._model.filter(id=id).delete()
         return deleted_count > 0
